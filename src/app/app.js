564e8c081f4e44a2b8ac999fa611919f4ac20db7
Yanan Guo <gynsolomon@gmail.com>
2015-01-17 12:10:22 +0800

    add video selector demo and readme file

--- src/app/app.js
+++ src/app/app.js
@@ -1,24 +1,60 @@
-angular.module( 'ngBoilerplate', [
+angular.module( 'selector', [
   'templates-app',
   'templates-common',
-  'ngBoilerplate.home',
-  'ngBoilerplate.about',
-  'ui.router'
+  'ui.bootstrap',
+  'ui.router',
+  'ngMaterial'
 ])
 
 .config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( '/home' );
+  $urlRouterProvider.otherwise( '/selector' );
+  $stateProvider.state('selector',{
+    url:'/selector',
+    controller: 'SelectorCtrl',
+    templateUrl: 'video_selector.tpl.html'
+  });
 })
 
 .run( function run () {
 })
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
-  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-    if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
-    }
-  });
+.controller( 'SelectorCtrl', function selectorCtrl ( $scope, $location,$sce ) {
+
+      $scope.init = function(){
+        $scope.chapter = {
+          name: '测试章节',
+          video: {
+            pianweishichang:'',
+            pianweishijian:  '',
+            url:'',
+            _id: ''
+          }
+        };
+
+        $scope.mockSource = [
+          {
+            name: '三角君的故事',
+            url: 'http://yc-course-video.qiniudn.com/5392b064c48cc97c366bc478'
+          }
+        ];
+
+        var getVideoAvInfo = function(videoUrl){
+          return {
+            pianweishichang: '30' //秒
+          }
+        };
+
+        $scope.selectVideo = function(video){
+          $scope.chapter.video.pianweishichang = getVideoAvInfo(video.url).pianweishichang;
+          $scope.chapter.video.url = video.url;
+          $sce.trustAsUrl($scope.chapter.video.url);
+          // TODO: $scope.chapter.video.pianweishijian ?? (duration - pianwei) 数学计算生成
+          // TODO: $scope.chapter.video._id ?? 传到后端由mongodb生成
+
+        };
+
+      };
+
 })
 
 ;
